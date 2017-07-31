feaa9e0f69084c2fa236e437b816567adbce716c
Nnyanzi Ian <nnyanziian@gmail.com>
2017-07-31 16:52:03 +0300

    fixed port issues

--- server.babel.js
+++ server.babel.js
@@ -48,7 +48,7 @@ io.on('connection', function (socket) {
     });
 
 });
-
-http.listen(3000, function() {
-  console.log(':3000');
+//.listen(process.env.PORT || 5000)
+http.listen(process.env.PORT || 3000, function() {
+  console.log('App started');
 });

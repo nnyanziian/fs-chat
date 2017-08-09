7814fdda972fd9ee623a48e6a6e71318aa41d9d8
Nnyanzi Ian <nnyanziian@gmail.com>
2017-08-09 10:03:38 +0300

    cors added

--- server.babel.js
+++ server.babel.js
@@ -5,55 +5,41 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-
-
-
+function handleErrors(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
 
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-//app.get('/about', function(req, res){
-   // res.send('Your on '+res);
-//});
-
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 io.on('connection', function (socket) {
-   // console.log('a user has connected');
-   //io.emit('chat message', "user connected");
-  // socket.on('disconnect', function(msg){
-      //console.log('user disconnected');
-  //io.emit('chat notification', msg);
- //  });
 
-    //get emitted chat msg
-   // socket.broadcast.emit('chat message');
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
 
-
-  //  socket.broadcast.emit('chat out');
     socket.on('chat out', function () {
-       
+
         io.emit('chat out');
     });
 
-      //  socket.broadcast.emit('chat typing');
     socket.on('chat typing', function (msg) {
-       
+
         io.emit('chat typing', msg);
     });
 
-      //  socket.broadcast.emit('chat not');
     socket.on('chat not', function (msg) {
-        //console.log('notification: '+msg.content);
-     io.emit('chat not', msg);
+
+        io.emit('chat not', msg);
     });
-
-
 });
-//.listen(process.env.PORT || 5000)
-http.listen(process.env.PORT || 3000, function() {
-  console.log('App started');
+http.listen(process.env.PORT || 3000, function () {
+    console.log('App started');
 });
--- src/model.js
+++ src/model.js
@@ -1,15 +1,69 @@
-var mongoClient = require('mongodb').MongoClient;
+//load mongoose package
+var mongoose = require('mongoose');
 
-var url = "mongodb://localhost:27017/fschat";
+var Schema = mongoose.Schema;
 
-mongoClient.connect(url, function (err, db) {
-    if (err) throw err
-        var myquery ={username:'Kasibante'};
-    db.collection('users').deleteOne(myquery, function (err, res) {
-        if (err) throw err;
-        console.log(JSON.stringify(res));
-        db.close();
+//connect to mongo Db and use fsChat DB
+mongoose.connect('mongodb://localhost/fschat');
 
-    });
 
+// Create a Schema that validates the input
+
+var UserSchema = new Schema({
+    "username": String,
+    "userPassword": String,
+    "userStatus": String,
+    "created": { type: Date, default: Date.now },
+    "online": Boolean
 });
+
+// Create a model based on the schema
+var User = mongoose.model('User', UserSchema);
+
+
+
+
+var callback = function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    else {
+        res.json(data);
+    }
+
+}
+
+export function getAll(res) {
+    User.find({}, function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        else {
+            res.json(data);
+        }
+    });
+}
+
+export function addUser(res, req) {
+    User.create(req.body,function (err, post) {
+        if (err)
+            console.log(err);
+        else{
+
+            res.json(post);
+        }
+         
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
